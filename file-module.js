b71fbc7906ab521e56bf5dad4eb99624f887158a
Vasanth1234567 <vasanthprakashvp@gmail.com>
2024-07-08 22:13:14 +0530

    Initial commit done.

---
+++ file-module.js
@@ -0,0 +1,29 @@
+const fs=require('fs')
+const filepath='./file.txt';
+const createFile=()=>{
+    fs.writeFileSync(filepath,'Hello World','utf8');
+    console.log("File Created.")
+}
+const readFile=()=>{
+    const fileData=fs.readFileSync(filepath,'utf8');
+    console.log(`Data in file : ${fileData}`);
+}
+const writeFile=(newContent)=>{
+    fs.writeFileSync(filepath,newContent,'utf8')
+    console.log('File Written.')
+}
+const updateFile=(appendContent)=>{
+    fs.appendFileSync(filepath,appendContent,'utf8')
+    console.log('File Updated')
+}
+const deleteFile=()=>{
+    fs.unlinkSync(filepath)
+    console.log('Deleted Successfully')
+}
+createFile()
+readFile()
+writeFile('Hi ')
+readFile()
+updateFile('Vasanth')
+readFile()
+deleteFile();
