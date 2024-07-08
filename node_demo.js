d452542b627286402134c5b406ed5533ff3bb933
Vasanth1234567 <vasanthprakashvp@gmail.com>
2024-07-08 11:57:18 +0530

    Initial COmmit DOne.

---
+++ node_demo.js
@@ -0,0 +1,15 @@
+const http =require('http')
+
+const server=http.createServer( (request,response)=>{
+    response.statusCode=200
+    response.setHeader('Content-Type','application/json')
+    const jsonResponse={message:`Hello World!`}
+    response.end(JSON.stringify(jsonResponse))
+})
+
+const hostname='127.0.0.1'
+const port=3500
+
+server.listen(port,hostname,()=>{
+    console.log(`Server started running at http://${hostname}:${port}/`)
+})
